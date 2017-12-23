172ff483b78ae10eac4f76df0a8c1c598cf692e0
va4ik <dmasterfake@gmail.com>
2017-12-24 01:58:34 +0300

    low power minor fix

--- src/modules/main/main-controller.js
+++ src/modules/main/main-controller.js
@@ -27,7 +27,22 @@ export default function mainCtrl($scope, $state, $bluetooth, $loading){
             step: 1,
             id: 'slider-id1',
             onChange: function(id, newVal) {
-                // $scope.send(LIGHT_BOTH+';'+newVal+';');
+                $scope.send(LIGHT_BOTH+';'+newVal+';')
+
+            },
+        }
+    };
+
+    $scope.leftSlider = {
+        value: 0,
+        options: {
+            floor: 0,
+            ceil: 255,
+            step: 1,
+            id: 'slider-id2',
+            onChange: function(id, newVal) {
+                $scope.send(LIGHT_L+';'+newVal+';');
+
                     // .then(function () {
                     //     $scope.send(newVal);
                     // }, function (err) {
@@ -37,117 +52,98 @@ export default function mainCtrl($scope, $state, $bluetooth, $loading){
         }
     };
 
-    // $scope.leftSlider = {
-    //     value: 0,
-    //     options: {
-    //         floor: 0,
-    //         ceil: 255,
-    //         step: 1,
-    //         id: 'slider-id2',
-    //         onChange: function(id, newVal) {
-    //             $scope.send(LIGHT_L+';'+newVal+';');
-    //
-    //                 // .then(function () {
-    //                 //     $scope.send(newVal);
-    //                 // }, function (err) {
-    //                 //     console.log(err);
-    //                 // })
-    //         },
-    //     }
-    // };
-    //
-    // $scope.rightSlider = {
-    //     value: 0,
-    //     options: {
-    //         floor: 0,
-    //         ceil: 255,
-    //         step: 1,
-    //         id: 'slider-id3',
-    //         onChange: function(id, newVal) {
-    //             $scope.send(LIGHT_R+';'+newVal+';')
-    //                 // .then(function () {
-    //                 //     $scope.send(newVal);
-    //                 // }, function (err) {
-    //                 //     console.log(err);
-    //                 // })
-    //         },
-    //     }
-    // };
- //    $bluetooth.isEnabled()
- //        .then($bluetooth.getBoundedDevices, function (err) {
- //            $bluetooth.enable()
- //                .then($bluetooth.getBoundedDevices)
- //                .then(function (devices) {
- //                    return $scope.devices = devices;
- //                })
- //        }).then(function (devices) {
- //            return $scope.devices = devices;
- //        });
- //
- //    $scope.openPort = function() {
- //        $loading.setStatus(true);
- //        return $bluetooth.subscribe('\n', $scope).then(function () {
- //            $loading.setStatus(false);
- //            return $scope.$on('$bluetooth-data', function (e, data) {
- //                $scope.bluetoothData = data;
- //            })
- //        });
- //    };
- //
- //    /*
- //        unsubscribes from any Bluetooth serial listener and changes the button:
- //    */
- //    $scope.closePort = function() {
- //        $loading.setStatus(true);
- //        return $bluetooth.unsubscribe($scope).then(function(){
- //            $loading.setStatus(false);
- //            return $scope.$on('$bluetooth-data-unsubscribe', function (e, data) {
- //                $scope.bluetoothData = data;
- //            })
- //        });
- //    };
- //
- //    $scope.send = function (text) {
- //        $bluetooth.write(text).then(function (success) {
- //            $scope.test = success;
- //        }, function (err) {
- //            $scope.test = err;
- //        });
- //    };
- //
- //
- //    $scope.manageConnection= function(address, $index) {
- //
- //        // connect() will get called only if isConnected() (below)
- //        // returns failure. In other words, if not connected, then connect:
- //        var connect = function () {
- //            // if not connected, do this:
- //            // clear the screen and display an attempt to connect
- //            $loading.setStatus(true);
- //            $bluetooth.connect(address)
- //                .then($scope.openPort, function (err) {
- //                    $loading.setStatus(false);
- //                })
- //                .then(function () {
- //                    $loading.setStatus(false);
- //                    $scope.devices[$index].active = !$scope.devices[$index].active;
- //                });
- //        };
- //
- //        // disconnect() will get called only if isConnected() (below)
- //        // returns success  In other words, if  connected, then disconnect:
- //        var disconnect = function () {
- //            $loading.setStatus(true);
- //            $bluetooth.disconnect()
- //                .then($scope.closePort, function (err) {
- //                    $loading.setStatus(false);
- //                })
- //                .then(function () {
- //                    $loading.setStatus(false);
- //                    $scope.devices[$index].active = !$scope.devices[$index].active;
- //                })
- //        };
- //        $bluetooth.isConnected().then(disconnect, connect);
- //    };
+    $scope.rightSlider = {
+        value: 0,
+        options: {
+            floor: 0,
+            ceil: 255,
+            step: 1,
+            id: 'slider-id3',
+            onChange: function(id, newVal) {
+                $scope.send(LIGHT_R+';'+newVal+';')
+                    // .then(function () {
+                    //     $scope.send(newVal);
+                    // }, function (err) {
+                    //     console.log(err);
+                    // })
+            },
+        }
+    };
+    $bluetooth.isEnabled()
+        .then($bluetooth.getBoundedDevices, function (err) {
+            $bluetooth.enable()
+                .then($bluetooth.getBoundedDevices)
+                .then(function (devices) {
+                    return $scope.devices = devices;
+                })
+        }).then(function (devices) {
+            return $scope.devices = devices;
+        });
+
+    $scope.openPort = function() {
+        $loading.setStatus(true);
+        return $bluetooth.subscribe('\n', $scope).then(function () {
+            $loading.setStatus(false);
+            return $scope.$on('$bluetooth-data', function (e, data) {
+                $scope.bluetoothData = data;
+            })
+        });
+    };
+
+    /*
+        unsubscribes from any Bluetooth serial listener and changes the button:
+    */
+    $scope.closePort = function() {
+        $loading.setStatus(true);
+        return $bluetooth.unsubscribe($scope).then(function(){
+            $loading.setStatus(false);
+            return $scope.$on('$bluetooth-data-unsubscribe', function (e, data) {
+                $scope.bluetoothData = data;
+            })
+        });
+    };
+
+    $scope.send = function (text) {
+        $bluetooth.write(text).then(function (success) {
+            $scope.test = success;
+        }, function (err) {
+            $scope.test = err;
+        });
+    };
+
+
+    $scope.manageConnection= function(address, $index) {
+
+        // connect() will get called only if isConnected() (below)
+        // returns failure. In other words, if not connected, then connect:
+        var connect = function () {
+            // if not connected, do this:
+            // clear the screen and display an attempt to connect
+            $loading.setStatus(true);
+            $bluetooth.connect(address)
+                .then($scope.openPort, function (err) {
+                    $loading.setStatus(false);
+                })
+                .then(function () {
+                    $loading.setStatus(false);
+                    $scope.devices[$index].active = !$scope.devices[$index].active;
+                });
+        };
+
+        // disconnect() will get called only if isConnected() (below)
+        // returns success  In other words, if  connected, then disconnect:
+        var disconnect = function () {
+            $loading.setStatus(true);
+            $bluetooth.disconnect()
+                .then($scope.closePort, function (err) {
+                    $loading.setStatus(false);
+                })
+                .then(function () {
+                    $loading.setStatus(false);
+                    $scope.devices[$index].active = !$scope.devices[$index].active;
+                })
+        };
+        $bluetooth.isConnected().then(disconnect, connect);
+    };
 
 }
