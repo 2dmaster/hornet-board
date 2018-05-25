b09702d6f0cc730b4d44a72e3c1275d9981d4e80
2dmaster <dmasterfake@gmail.com>
2018-05-25 14:43:43 +0300

    cr2

--- firmware/hornet-board_v1.0/hornet-board_v1.0.ino
+++ firmware/hornet-board_v1.0/hornet-board_v1.0.ino
@@ -2,34 +2,26 @@
 // common constants
 #define ON 1
 #define OFF 20
-#define STROBE 999
 // end common constants
 
 // pin config
 #define BT_SPEED 38400
 #define BT_RX 2
 #define BT_TX 3
-#define AMP 4
-#define ILLUMINATION 8
-#define LIGHT_L 6
-#define TRUNK 5
-#define LIGHT_R 9
-#define LIGHT_BOTH 11
+#define BUZZER 4
+#define DOOR_LOCK 5
 #define VOLTMETER 7
 //end pin config 
 
-bool DO_STROBE = false;
+
 SoftwareSerial BT(BT_RX,BT_TX);
 
 unsigned long vMeasureLast = 0;
 
 void setup()
 {
-  pinMode(AMP, OUTPUT);
-  pinMode(ILLUMINATION, OUTPUT);
-  pinMode(TRUNK, OUTPUT);
-  pinMode(LIGHT_L, OUTPUT);
-  pinMode(LIGHT_R, OUTPUT);
+  pinMode(BUZZER, OUTPUT); 
+  pinMode(DOOR_LOCK, OUTPUT); 
   pinMode(VOLTMETER, INPUT);
   
 //  Serial.setTimeout(100);
@@ -46,51 +38,6 @@ void processDigitalDevice(byte device){
     break;
   case OFF:
     digitalWrite(device, LOW);
-    break;
-  }
-}
-
-void strobe(bool enabled){
-  byte _delay = 80;
-  if (enabled){
-    Serial.println("strobe in");
-    digitalWrite(LIGHT_L, HIGH);
-    delay(_delay);
-    digitalWrite(LIGHT_L, LOW);
-    delay(_delay);
-    digitalWrite(LIGHT_L, HIGH);
-    delay(_delay);
-    digitalWrite(LIGHT_L, LOW);
-    delay(_delay);
-    digitalWrite(LIGHT_R, HIGH);
-    delay(_delay);
-    digitalWrite(LIGHT_R, LOW);
-    delay(_delay);
-    digitalWrite(LIGHT_R, HIGH);
-    delay(_delay);
-    digitalWrite(LIGHT_R, LOW);
-  }   
-}
-
-void processAnalogDevice(int device){  
-  int brightness = BT.parseInt();
-  switch (device) {
-  case LIGHT_L:
-    DO_STROBE = false;
-    analogWrite(device, brightness);
-    break;
-  case LIGHT_R:
-    DO_STROBE = false;
-    analogWrite(device, brightness);
-    break;
-  case LIGHT_BOTH:
-    if (brightness == STROBE){
-      DO_STROBE = true;
-    } else {
-      DO_STROBE = false;
-      analogWrite(LIGHT_L, brightness);
-      analogWrite(LIGHT_R, brightness);  
-    }  
     break;
   }
 }
@@ -115,19 +62,12 @@ void loop()
     device = BT.parseInt();     
     if (device != 0){
       switch (device) {
-        case AMP:
-        case TRUNK:
-        case ILLUMINATION:
+        case BUZZER:
+        case DOOR_LOCK:
           processDigitalDevice(device);
-          break;  
-        case LIGHT_L:
-        case LIGHT_R:
-        case LIGHT_BOTH:         
-          processAnalogDevice(device);
           break;
       } 
     }    
   }
-  strobe(DO_STROBE);
   measureVolts(1000);
 }
